ebb6a84d254649b3144263e657fc89b7e0097163
seegler <seegler@gmail.com>
2015-07-27 01:05:57 +0530

    Add documentation in Shiny app

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# DevelopingDataProducts - Course Project
-
+DevelopingDataProducts - Course Project
+=========================================
 This is a submission for Coursera: Developing Data Products- Course Project
 
-#GDP Explorer Application
+GDP Explorer Application
+========================
 This application is based on the data available from World Bank's API server. The dataset has been downloaded from http://api.worldbank.org/v2/en/indicator/ny.gdp.mktp.cd?downloadformat=csv and added to the project under the data folder.
 
 Application can present the GDP of countries as a line plot. On the application page, user can adjust the range of years from 1960 to 2014 and select multiple countries from a list of countries.
--- ui.R
+++ ui.R
@@ -1,17 +1,23 @@
 library(shiny)
 library(ggplot2)
 library(rCharts)
-
-shinyUI(pageWithSidebar(
-  headerPanel("Explore GDP Growth of Countries"),
-  sidebarPanel(
-    sliderInput("input_years", "Select Years:", min = 1960, max = 2014,
-                value = c(2001, 2010)),
-    selectInput("input_countries", "Select Countries:", "", 
-                multiple = TRUE, selectize = TRUE)
-  ),
-  mainPanel(
-   plotOutput("gdpPlot")
+library(markdown)
+shinyUI(
+  navbarPage("Explore GDP Growth of Countries",
+    tabPanel("Run",
+        sidebarPanel(
+          sliderInput("input_years", "Select Years:", min = 1960, max = 2014,
+                      value = c(2001, 2010)),
+          selectInput("input_countries", "Select Countries:", "", 
+                      multiple = TRUE, selectize = TRUE)
+        ),
+        mainPanel(
+         plotOutput("gdpPlot")
+        )
+    ),
+    tabPanel("Help",
+        mainPanel(includeMarkdown("README.md"))
+    )
   )
-))
+)
   
