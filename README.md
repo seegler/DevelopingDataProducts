7606286f48489ecaff13e5fc68a8b19952350d26
seegler <seegler@gmail.com>
2015-07-27 00:40:02 +0530

    Update ShinyApp and RPubs links

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# DevelopingDataProducts
+# DevelopingDataProducts - Course Project
+
+This is a submission for Coursera: Developing Data Products- Course Project
+
+#GDP Explorer Application
+This application is based on the data available from World Bank's API server. The dataset has been downloaded from http://api.worldbank.org/v2/en/indicator/ny.gdp.mktp.cd?downloadformat=csv and added to the project under the data folder.
+
+Application can present the GDP of countries as a line plot. On the application page, user can adjust the range of years from 1960 to 2014 and select multiple countries from a list of countries.
+
+Application is available at https://seegler.shinyapps.io/DevelopingDataProducts
+
+Detailed presentation of the application is available at http://rpubs.com/seegler/DevelopingDataProductsProject
+
--- server.R
+++ server.R
@@ -1,6 +1,6 @@
 library(shiny)
 require(reshape2)
-#
+#load data
 data <- read.csv("./data/ny.gdp.mktp.cd_Indicator_en_csv_v2.csv", skip=4, header=TRUE, check.names = FALSE);
 colnames(data)[1]="Country"
 
@@ -9,16 +9,20 @@ shinyServer(
     
     #reactive input processing
     plot_data <- reactive({
-      
+      #columns to subset the data
+      #columns subset includes Country and the range of years 
+      #each year is a column in World Bank data
       columns <- c("Country", input$input_years[1]:input$input_years[2]);
       input_countries <- input$input_countries;
       
       plot_data <-data[data$Country %in% input_countries,columns]
       #reshape columns for plotting
+      #This reshapes the data structure byu removing all the Year columns
+      # and adding them as rows. This helps easy plaotting
       plot_data <- melt(plot_data)
       colnames(plot_data) = c("Country","Year","GDP");
       #convert to billion
-      plot_data[3] = plot_data[3]/100000000
+      plot_data[3] = plot_data[3]/1000000000
       plot_data
     })
     ## output ggplot
@@ -29,7 +33,7 @@ shinyServer(
         ylab("GDP in billion $") + 
         ggtitle("GDP Growth"))
     });
-    #bind option values of countries selectio box to the session
+    #bind option values of countries selection box to the session
     observe({
       updateSelectInput(session, "input_countries", 
                         choices = as.character(data[[1]]),
